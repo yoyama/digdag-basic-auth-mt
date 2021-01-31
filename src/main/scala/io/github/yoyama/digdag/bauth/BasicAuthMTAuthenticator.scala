6d7ebd54447c31a8e3c4841552c937e9e355b822
You Yamagata <youy@bg8.so-net.ne.jp>
2021-01-31 15:58:04 +0900

    Support the new config key from Digdag v0.10.0.0

--- src/main/scala/io/github/yoyama/digdag/bauth/BasicAuthMTAuthenticator.scala
+++ src/main/scala/io/github/yoyama/digdag/bauth/BasicAuthMTAuthenticator.scala
@@ -1,26 +1,35 @@
 package io.github.yoyama.digdag.bauth
 
-import java.nio.file.{FileSystems, Path, Paths}
 
+import java.nio.file.{FileSystems, Path, Paths}
 import com.google.inject.Inject
-import io.digdag.client.config.Config
-import io.digdag.spi.Authenticator
+import io.digdag.client.config.{Config, ConfigFactory}
+import io.digdag.spi.{AuthenticatedUser, Authenticator}
 import io.digdag.spi.Authenticator.Result
+
 import javax.ws.rs.container.ContainerRequestContext
 import org.slf4j.{Logger, LoggerFactory}
 
-class BasicAuthMTAuthenticator @Inject() (systemConfig:Config) extends Authenticator {
+
+class BasicAuthMTAuthenticator @Inject() (systemConfig:Config, cf: ConfigFactory) extends Authenticator {
   private val logger = LoggerFactory.getLogger(classOf[BasicAuthMTAuthenticator])
   private val passwdFileName:String = systemConfig.get("plugins.basic_auth_mt.passwd_file", classOf[String])
   private val userMapFileName:String = systemConfig.get("plugins.basic_auth_mt.user_map_file", classOf[String])
   private val dao:BasicAuthDAO = new BasicAuthFileDAO(toPath(passwdFileName),toPath(userMapFileName))
 
+  def toDigdagAutenticatedUser(userInfo:UserInfo):AuthenticatedUser = {
+    AuthenticatedUser.builder()
+      .siteId(userInfo.siteId)
+      .isAdmin(userInfo.admin)
+      .userInfo(cf.create())
+      .userContext(cf.create())
+      .build()
+  }
+
   override def authenticate(requestContext: ContainerRequestContext): Authenticator.Result = {
     dao.authorize(Option(requestContext.getHeaderString("Authorization"))) match {
-      case Some(userInfo) => Result.builder.isAdmin(userInfo.admin).siteId(userInfo.siteId).build
-      case _ => {
-        Result.reject("Failed to authorize")
-      }
+      case Some(userInfo) => Result.accept(toDigdagAutenticatedUser(userInfo))
+      case _              => Result.reject("Failed to authorize")
     }
   }
 
--- src/main/scala/io/github/yoyama/digdag/bauth/BasicAuthMTAuthenticatorPlugin.scala
+++ src/main/scala/io/github/yoyama/digdag/bauth/BasicAuthMTAuthenticatorPlugin.scala
@@ -1,13 +1,16 @@
 package io.github.yoyama.digdag.bauth
 
 
-import com.google.inject.{Binder, Inject, TypeLiteral}
-import io.digdag.spi.{Authenticator, Plugin}
+import com.google.inject.Inject
+import io.digdag.spi.{Authenticator, AuthenticatorFactory, Plugin}
 
 class BasicAuthMTAuthenticatorPlugin @Inject() (/**systemConfig: Config*/) extends Plugin {
   override def getServiceProvider[T](t: Class[T]): Class[_ <: T] = {
     if (t eq classOf[Authenticator]) {
       classOf[BasicAuthMTAuthenticator].asSubclass(t)
+    }
+    else if (t eq classOf[AuthenticatorFactory]) {
+      classOf[BasicAuthMTAuthenticatorFactory].asSubclass(t)
     }
     else {
       null
